f41c8788005f30df86dec13dbda3bdde8c3336b5
tracer-ics2020 <tracer@njuics.org>
2022-05-08 23:09:46 +0800

    >  compile 191220000    Static hostname: DESKTOP-J38TP5F          Icon name: computer-container            Chassis: container         Machine ID: 8da23387699ee4fbbab0fb3f62700039            Boot ID: d1df01769d454b3f9c4408c29b7f4260     Virtualization: wsl   Operating System: Ubuntu 20.04 LTS             Kernel: Linux 5.10.102.1-microsoft-standard-WSL2       Architecture: x86-64  23:09:46 up  8:18,  0 users,  load average: 0.06, 0.07, 0.01

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -104,10 +104,10 @@ void trace_watchpoints(bool *stop)
 void watchpoints_display()
 {
   WP *wp = head;
-  printf("| No \t| What \t\t| Value \t|\n");
+  printf("| No \t| What \t\t| Value \t\t|\n");
   while (wp != NULL)
   {
-    printf("| %d \t| %s \t| 0x%x \t|\n", wp->NO, wp->expr, wp->prev_val);
+    printf("| %d \t| %s \t| 0x%08x \t|\n", wp->NO, wp->expr, wp->prev_val);
     wp = wp->next;
   }
 }
